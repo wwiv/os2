48a49f78cf7b520dfd5486db437a545759122560
Rushfan <rushfan@wwivbbs.org>
2021-05-03 00:01:19 -0500

    added gcc11 env

---
+++ gcc/gcc11env.cmd
@@ -0,0 +1,6 @@
+set PATH=C:\usr\local11\libexec\gcc\i686-pc-os2-emx\11.1.0;C:\usr\local11\bin;%PATH%
+set BEGINLIBPATH=C:\usr\local11\lib;C:\usr\lib;%BEGINLIBPATH%
+set LIBPATHSTRICT=T
+set C_INCLUDE_PATH=C:/usr/local11/lib/gcc/i686-pc-os2-emx/11.1.0/include;C:/usr/local11/include;C:/usr/include;%C_INCLUDE_PATH%
+set CPLUS_INCLUDE_PATH=C:/usr/local11/include/c++/11.1.0;C:/usr/local11/include/c++/11.1.0/i686-pc-os2-emx;C:/usr/local11/include/c++/11.1.0/backward;C:/usr/local11/include;C:/usr/include;%CPLUS_INCLUDE_PATH%
+set LIBRARY_PATH=C:/usr/local11/lib/gcc/i686-pc-os2-emx/11.1.0;C:/usr/local11/lib;C:/usr/lib;%LIBRARY_PATH%
