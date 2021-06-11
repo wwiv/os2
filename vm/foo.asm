fea8fe5c37c21895d3dd8c90b0a31f29fb1d26b9
Rushfan <rushfan@wwivbbs.org>
2021-06-11 00:29:47 -0500

    Cleaned up asm code

--- vm/foo.asm
+++ vm/foo.asm
@@ -10,8 +10,7 @@ BDOOR_MAGIC	equ	564D5868H
 	
 ;; Low-bandwidth backdoor port number 
 ;; for the IN/OUT interface.
-BDOOR_PORT	equ	
-	5658H
+BDOOR_PORT	equ	5658H
 
 _DATA   ends
 
@@ -20,22 +19,15 @@ assume  cs:_TEXT,ds:DGROUP
 _TEXT   segment word public 'CODE'
         assume  CS:_TEXT
 
-        public  RetP_
-RetP_   proc    near   ; int Ret4()
-	add     EAX, 1
-        ret
-RetP_   endp
-
+	;; int Backdoor(int)
 	public Backdoor_
-Backdoor_	proc	near	;  int Backdoor(int)
-		
-		mov eax, 564D5868H
+Backdoor_	proc	near
+ 		mov ecx, eax
+		mov eax, BDOOR_MAGIC
 		mov ebx, 0
-		mov ecx, 6h
-		mov dx, 5658H
+		mov dx, BDOOR_PORT
 		in eax, dx
-		nop
-		add eax, 1
+		ret
 Backdoor_	endp
 	
 _TEXT   ends
--- vm/vm.c
+++ vm/vm.c
@@ -1,43 +1,10 @@
 #include <stdio.h>
 
-extern int Ret4();
-extern int RetP(int);
 extern int Backdoor(int);
-
-// MAGIC number to send to backdoor api
-#define BDOOR_MAGIC 0x564D5868;
-
-// Low-bandwidth backdoor port number 
-// for the IN/OUT interface.
-#define BDOOR_PORT 0x5658;
-#define BDOOR_MAGIC_T ~BDOOR_MAGIC
-int b(int cmd) {
-  long result = 1;
-  long t = BDOOR_MAGIC_T;
-  _asm {
-       push edx
-       push ecx
-       push ebx
-       mov ecx, cmd
-       mov eax, 564D5868h
-       mov dx, 5658h
-       in eax, dx
-       add eax, 21
-       mov result, eax
-       mov [result], 22
-       pop ebx
-       pop ecx
-       pop edx
-       }
-   return result;
-}
 
 int main(int argc, char* argv) {
   int i;
   puts("Hello world\r\n");
-
-  i = RetP(123);
-  printf("RetP: %d\r\n", i);
 
   i = Backdoor(6);
   printf("Selection Len (asm): %d\r\n", i);
